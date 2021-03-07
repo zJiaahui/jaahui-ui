dec947ee1890977ec79631e6c65bf951acb3d42e
zjiaahui <211535176@qq.com>
2021-03-07 13:09:04 +0800

    创建button-group组件

---
+++ src/button-group.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="jh-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JHButtonGroup",
+};
+</script>
+
+<style lang="scss">
+.jh-button-group {
+  display: inline-block;
+  vertical-align: middle;
+  font-size: 0;
+  > .jh-button {
+    border-radius: 0;
+    &:not(:first-child) {
+      margin-left: -1px;
+    }
+    &:first-child {
+      border-top-left-radius: var(--border-radius);
+      border-bottom-left-radius: var(--border-radius);
+    }
+    &:last-child {
+      border-top-right-radius: var(--border-radius);
+      border-bottom-right-radius: var(--border-radius);
+    }
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import JhIcon from "./icon.vue";
 import JHButton from "./button.vue"
+import JHButtonGroup from "./button-group.vue"
 Vue.component("jh-button", JHButton)
 Vue.component("jh-icon", JhIcon)
+Vue.component("jh-button-group", JHButtonGroup)
 new Vue({
     el: "#app",
     data: {
