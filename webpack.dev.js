7a8a1f8edbc75361a6243ba7bd0f835e327e31ce
zjiaahui <211535176@qq.com>
2021-03-14 15:17:39 +0800

    配置webpack的热更新devServerr

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,17 @@
 const { merge } = require('webpack-merge');
- const common = require('./webpack.common.js');
+const common = require('./webpack.common.js');
+const path = require('path');
 
- module.exports = merge(common, {
-   mode: 'development',
- });
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    //自动运行项目目录（构建后的目录）
+    contentBase: path.resolve(__dirname, 'dist'),
+    //启动gzip压缩（让代码体积更小速度更快）
+    compress: true,
+    //指定运行的端口号
+    port: 3001,
+    //打开默认浏览器
+    open: true
+  }
+});
