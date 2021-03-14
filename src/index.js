223f078b448ac6857feb0d9e831726dc417da401
zjiaahui <211535176@qq.com>
2021-03-14 23:21:04 +0800

    创建字体图标组件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue"
 import JButton from "./components/Button/j-button.vue"
+import JIcon from "./components/Icon/j-icon.vue"
 Vue.component("j-button", JButton)
+Vue.component("j-icon", JIcon)
 new Vue({
     el: "#app",
     data: {
