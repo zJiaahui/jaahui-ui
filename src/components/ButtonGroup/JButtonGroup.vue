7d94b7db878e61b696ba12eae138d1851639b5e6
zjiaahui <211535176@qq.com>
2021-03-15 18:01:32 +0800

    完成button-group组件

--- src/components/ButtonGroup/JButtonGroup.vue
+++ src/components/ButtonGroup/JButtonGroup.vue
@@ -1,8 +1,28 @@
-<template></template>
+<template>
+  <div class="j-button-group">
+    <slot></slot>
+  </div>
+</template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="scss" socped>
+.j-button-group {
+  font-size: 0;
+  & .j-button {
+    border-radius: 0;
+  }
+  & > .j-button:first-child {
+    border-radius: 4px;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+  & > .j-button:last-child {
+    border-radius: 4px;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+}
 </style>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import Vue from "vue"
 import JButton from "./components/Button/j-button.vue"
 import JIcon from "./components/Icon/j-icon.vue"
+import JButtonGroup from "./components/ButtonGroup/JButtonGroup.vue"
 Vue.component("j-button", JButton)
 Vue.component("j-icon", JIcon)
+Vue.component("j-button-group", JButtonGroup)
 new Vue({
     el: "#app",
     data: {
