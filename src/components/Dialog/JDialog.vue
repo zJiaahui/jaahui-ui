386e9122a68ae7deed1594728129b7e527f30153
zjiaahui <211535176@qq.com>
2021-03-16 23:20:19 +0800

    完成对话框组件

---
+++ src/components/Dialog/JDialog.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="j-wrapper" v-if="visible" @click.self="close">
+    <div class="j-dialog">
+      <div class="j-d-header">
+        <slot name="header">
+          <span>{{ title }}</span>
+        </slot>
+        <button @click="close">
+          <j-icon icon="error"></j-icon>
+        </button>
+      </div>
+      <div class="j-d-body">
+        <slot>
+          <span>这是一段内容</span>
+        </slot>
+      </div>
+      <div class="j-d-footer" v-if="$slots.footer">
+        <slot name="footer"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JDialog",
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    title: {
+      type: String,
+      default: "提示",
+    },
+  },
+  methods: {
+    close() {
+      this.$emit("update:visible", false);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.j-wrapper {
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(134, 130, 130, 0.5);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  overflow: auto;
+  .j-dialog {
+    background-color: white;
+    box-sizing: border-box;
+    min-width: 400px;
+    min-height: 200px;
+    display: flex;
+    flex-direction: column;
+    padding: 0 20px;
+
+    .j-d-header {
+      height: 50px;
+      line-height: 50px;
+      position: relative;
+      button {
+        background: transparent;
+        border: none;
+        outline: none;
+        cursor: pointer;
+        font-size: 16px;
+        position: absolute;
+        top: 15px;
+        right: 0px;
+        padding: 0;
+        width: 16px;
+        height: 16px;
+      }
+    }
+    .j-d-footer {
+      height: 50px;
+      line-height: 50px;
+      text-align: end;
+    }
+
+    .j-d-body {
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  }
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -4,16 +4,20 @@ import JIcon from "./components/Icon/JIcon.vue"
 import JButtonGroup from "./components/ButtonGroup/JButtonGroup.vue"
 import JSwitch from "./components/Switch/JSwitch.vue"
 import JInput from "./components/Input/JInput.vue"
+import JDialog from "./components/Dialog/JDialog.vue"
 Vue.component("j-button", JButton)
 Vue.component("j-icon", JIcon)
 Vue.component("j-button-group", JButtonGroup)
 Vue.component("j-switch", JSwitch)
 Vue.component("j-input", JInput)
+Vue.component("j-dialog", JDialog)
 new Vue({
     el: "#app",
     data: {
         msg: "你好",
         isLoading: false
         , ischecked: true
+        , visible: false
+        , visible2: false
     }
 })
