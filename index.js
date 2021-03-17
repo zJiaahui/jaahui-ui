e240c427f9e5a0d0bff89a11a0145a21ea16812e
zjiaahui <211535176@qq.com>
2021-03-17 21:27:55 +0800

    在生产环境打包入口文件中导入新完成的组件

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import Button from "./src/components/Button/JButton.vue"
 import ButtonGroup from "./src/components/ButtonGroup/JButtonGroup.vue"
 import Icon from "./src/components/icon/JIcon.vue"
-
-export { Button, ButtonGroup, Icon }
+import Switch from "./src/components/Switch/JSwitch.vue"
+import Input from "./src/components/Input/JInput.vue"
+import Dialog from "./src/components/Dialog/JDialog.vue"
+import Toast from "./src/components/Toast/Toast.js"
+import Pager from "./src/components/Pager/JPager.vue"
+export { Button, ButtonGroup, Icon, Switch, Input, Dialog, Toast, Pager }
