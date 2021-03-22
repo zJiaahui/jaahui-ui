ed6ccad51b35a45f25fb1583eb10d2954aeb3a9c
zjiaahui <211535176@qq.com>
2021-03-22 13:21:50 +0800

    安装兼容webpack4的CleanWebpackPlugin插件（构建前自动清理输出目录）并配置

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,19 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = merge(common, {
     mode: 'production',
     entry: {
         app: './index.js',
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'lib'),
-       // clean: true,
+        // clean: true,
     },
 });
